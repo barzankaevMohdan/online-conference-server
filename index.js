023a426eb43903e59b014e8df4a55864364b8c93
barzankaevMohdan <mohdan.barzankaev@gmail.com>
2022-03-21 14:41:41 +0300

    no-credentials

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    credentials: true,
+    // credentials: true,
     origin: '*'
 }))
 app.use('/api', router)
