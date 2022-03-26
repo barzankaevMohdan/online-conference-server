368fb11cf352039eeaa1ee9303df4e5aeca8677b
barzankaevMohdan <mohdan.barzankaev@gmail.com>
2022-03-27 01:41:17 +0300

    fix

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -15,6 +15,7 @@ class UserController {
                 'refreshToken',
                 userData.refreshToken,
                 {
+                    domain: 'mohdan-online-conference-front.herokuapp.com',
                     secure: true,
                     maxAge: 30 * 24 * 60 * 60 * 1000,
                     httpOnly: true,
@@ -35,6 +36,7 @@ class UserController {
                 'refreshToken',
                 userData.refreshToken,
                 {
+                    domain: 'mohdan-online-conference-front.herokuapp.com',
                     secure: true,
                     maxAge: 30 * 24 * 60 * 60 * 1000,
                     httpOnly: true,
@@ -77,6 +79,7 @@ class UserController {
                 'refreshToken',
                 userData.refreshToken,
                 {
+                    domain: 'mohdan-online-conference-front.herokuapp.com',
                     secure: true,
                     maxAge: 30 * 24 * 60 * 60 * 1000,
                     httpOnly: true,
