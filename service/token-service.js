6bba7e91655d41434fca872572fd608c98e0f9de
barzankaevMohdan <mohdan.barzankaev@gmail.com>
2022-03-26 17:46:10 +0300

    fix

--- service/token-service.js
+++ service/token-service.js
@@ -1,10 +1,21 @@
 const jwt = require('jsonwebtoken')
 const {Token} = require('../models/models')
 
+const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET ?? 'jwt-secret-key-mohdan'
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET ?? 'jwt-refresh-secret-key-mohdan'
+
 class TokenService {
     generateToken(payload) {
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '30m'})
-        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '30d'})
+        const accessToken = jwt.sign(
+            payload,
+            JWT_ACCESS_SECRET,
+            {expiresIn: '30m'}
+        )
+        const refreshToken = jwt.sign(
+            payload,
+            JWT_REFRESH_SECRET,
+            {expiresIn: '30d'}
+        )
         return {
             accessToken,
             refreshToken,
