bf7ea50994f89a310e47bcc4f3c1b64f7f5a8a32
barzankaevMohdan <mohdan.barzankaev@gmail.com>
2022-03-28 23:07:24 +0300

    ss

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(session({
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: 'none',
+        domain: process.env.DOMAIN,
     }
 }))
 app.use(express.json())
