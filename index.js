514aaf745d93aa400f5c648f87306d9149b076b3
barzankaevMohdan <mohdan.barzankaev@gmail.com>
2022-03-31 14:36:56 +0300

    ss

--- index.js
+++ index.js
@@ -14,13 +14,11 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     secret: 'sessionss',
-    secureProxy: true,
     cookie: {
         secure: true,
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: 'none',
-        domain: process.env.DOMAIN,
     }
 }))
 app.use(express.json())
