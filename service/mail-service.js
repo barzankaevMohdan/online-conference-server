6edc0f037876581a1350ff81e8b2673e75f17728
barzankaevMohdan <mohdan.barzankaev@gmail.com>
2022-04-29 01:53:37 +0300

    return mailer settings

--- service/mail-service.js
+++ service/mail-service.js
@@ -4,10 +4,9 @@ class mailService {
 
     constructor() {
         this.transporter = nodemailer.createTransport({
-            // host: process.env.SMTP_HOST,
-            // port:process.env.SMTP_PORT,
-            // secure:false,
-            service: 'gmail',
+            host: process.env.SMTP_HOST,
+            port:process.env.SMTP_PORT,
+            secure:false,
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASSWORD
